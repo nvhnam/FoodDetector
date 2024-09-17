612cc43e77d9ef75cb55e5a6091af8590a33de24
Nam <nvhnam01@gmail.com>
2024-09-17 18:01:11 +0700

    Change Youtube importer to pafy

--- ultralytics/data/loaders.py
+++ ultralytics/data/loaders.py
@@ -523,7 +523,7 @@ def autocast_list(source):
     return files
 
 
-def get_best_youtube_url(url, method="pytube"):
+def get_best_youtube_url(url, method="pafy"):
     """
     Retrieves the URL of the best quality MP4 video stream from a given YouTube video.
 
